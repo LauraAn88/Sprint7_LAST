3180a06cab33d12a2fb5a70e705d46831825bf57
LauraAn88 <293963589+LauraAn88@users.noreply.github.com>
2026-06-24 17:39:53 -0600

    Agregar gráfico de dispersión y ajustes a readme

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 Proyecto sprint 7
 
 URL Render: https://sprint7-last.onrender.com
+
+El proyecto de sprint 7 tiene cómo finalidad explotar los conocimientos adquiridos hasta ahora:
+- Limpieza de datos en python:
+    Se utilizan librería pandas pandas la lectura, limpieza y exploración de información.
+    Librería plotly para la visualización de gráficos
+    Y streamlit para la conversión en aplicativos web 
+- Herramientas github para el versionamiento de los proyectos y el alcance web
+    Se utiliza para el versionamiento de los proyectos y la conexión con render
+- Manejo de aplicativos para productivizar aplicativos web
+    Render es una herramienta que te permite lanzar el aplicativo web 
+
+El resultado de este proyecto es utilizar las herramientas mencionados anteriormente para visualizar en la web un aplicativo que te permite explorar dos gráficos del insumo vehicules.
--- app.py
+++ app.py
@@ -10,6 +10,9 @@ ruta_csv = Path(
     'C:/Users/Usuario/OneDrive/Desktop/TripleTen/APPs/Sprint7_LAST') / 'vehicles_us_mod.csv'
 vehicles_us = pd.read_csv('vehicles_us_mod.csv')
 
+# Crea un encabezado
+st.header('Gráficos: Anuncios de venta de coches')
+
 # Crear un botón en la aplicación Streamlit
 hist_button = st.button('Construir histograma')
 
@@ -29,3 +32,23 @@ if hist_button:
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
     st.plotly_chart(fig, use_container_width=True)
+
+
+# Crear una casilla de verificación
+build_dispersion = st.checkbox('Gráfico de dispersión')
+
+if build_dispersion:  # si la casilla de verificación está seleccionada
+    st.write(
+        'Construir un gráfico de dispersión identificar la relación odómetro vs precio')
+
+    # Crear un scatter plot utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de scatter
+    fig2 = go.Figure(data=[go.Scatter(
+        x=vehicles_us['odometer'], y=vehicles_us['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig2.update_layout(title_text='Relación entre Odómetro y Precio')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig2, use_container_width=True)
